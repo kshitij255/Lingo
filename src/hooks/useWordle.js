8a84dfcb6c5532531f977c754131cd54970b9887
Kshitij Waghmare <kshitij.rw@sintecmedia.com>
2023-07-18 11:01:17 +0530

    finished handle keyup function

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -7,11 +7,34 @@ const useWordle = (solution) => {
   const [history, setHistory] = useState([]); // each guess is a string
   const [isCurrent, setIsCurrent] = useState(false);
 
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("formatGuess", currentGuess);
+  };
 
   const addNewGuess = () => {};
 
+  //handle keyup event and track current guess
+  // if user presses enter add new guess
   const handleKeyUp = ({ key }) => {
+    if (key === "Enter") {
+      //only add new guess if turn is less than 5
+      if (turn > 5) {
+        console.log("used all guesses");
+        return;
+      }
+      //do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("already tried that word");
+        return;
+      }
+      // check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log("word must be 5 chars long");
+        return;
+      }
+      formatGuess();
+    }
+
     if (key === "Backspace") {
       setCurrentGuess((prev) => {
         return prev.slice(0, -1);
